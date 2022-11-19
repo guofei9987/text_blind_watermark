00fa3a72166afd1edb974358e8cc3010871942ac
guofei9987 <guofei9987@foxmail.com>
2022-11-19 22:03:32 +0800

    add: extract from file

---
+++ examples/example_extract_from_file.py
@@ -0,0 +1,11 @@
+# 展示如何从打水印的文本文件中提取盲水印
+from text_blind_watermark import TextBlindWatermark
+
+password = '20190808'
+with open('file.txt', 'r') as f:
+    text_embed = f.read()
+
+twm_new = TextBlindWatermark(password=password)
+wm_extract = twm_new.extract(text_embed)
+print("解出的盲水印：")
+print(wm_extract)
